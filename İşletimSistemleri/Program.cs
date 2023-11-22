5adf21cd733a06a828a541003c1ba49f131df5c7
Yusuf Ziya Birgül <110490415+YusufZiyaBirgul@users.noreply.github.com>
2023-11-22 11:25:41 +0300

    işletim 2023

--- İşletimSistemleri/Program.cs
+++ İşletimSistemleri/Program.cs
@@ -19,11 +19,11 @@ namespace İşletimSistemleri
         {
             for (int i = 0; i < 10000; i++)
             {
-                //Console.Write("y"); //Kritik Alan
+                Console.Write("y"); //Kritik Alan
 
-                //lock (obj) //THREAD SAFE kod yazılacağı zaman yazılır
+                lock (obj)  //THREAD SAFE kod yazılacağı zaman yazılır
 
-                //sayi++;
+                    sayi++;
 
                 //dizi[(int)indis]++; //dizi değişkenin 10.indisini 10.000 arttırıyor.
             }
@@ -60,7 +60,18 @@ namespace İşletimSistemleri
             }
         }
 
-        //--------------------------------------------------------------------------
+        static int arttirSayac = 0;
+
+        static void arttir()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                Thread.Sleep(1);
+                sayac += i;
+            }
+        }
+
+        //-------------------------------------------------------------------------- DEADLOCK
         static object o1 = new object();
         static object o2 = new object();
         static void y1()
@@ -84,12 +95,13 @@ namespace İşletimSistemleri
                     Console.WriteLine("4");
             }
         }
-        //--------------------------------------------------------------------------
+
+        //-------------------------------------------------------------------------- Semaphore // alttaki semaphore kodları daha basit anlatıyor, bu çok karmaşık
 
         //Main thread beklerken bu fonksiyon çalışıyor
         private static Semaphore _pool;
         private static int _padding;
-        private static void Worker(object num) //Semaphore
+        private static void Worker(object num) //Semaphore 
         {
             Console.WriteLine("Thread {0} begins" + "and waits for the semaphore", num);
             _pool.WaitOne();//K.A. girerken Semaphore un WaitOne fonk. kullanılır.
@@ -120,14 +132,90 @@ namespace İşletimSistemleri
             //K.A dan çıkarken Relase() fonk. kullanılır.
         }
 
+        //------------------------------- Interlocked
+
+        static int sayac = 0;
+        private static void arttirInterlocked()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                //Interlocked.Add(ref sayac, 10);         // belirtilen değeri(10) sayaca değişkenine ekler
+                //Interlocked.Increment(ref sayac);       // sayacı atomik bir şekilde bir arttırır.
+                //Interlocked.Exchange(ref sayac, 10);    // sayacın değerini belirtilen değerle(10) değiştirir ve eski değeri döndürür.
+
+            }
+        }
+
+        //--------------------------Interlocked Sınav Sorusu (2022)
+
+        static int sayacBir = 0;
+        static int sayacSıfır = 0;
+
+        static void doluBosKontrol(int[] dizi, int baslangic, int bitis)
+        {
+            for (int i = baslangic; i < bitis; i++)
+            {
+                if (dizi[i] == 1)
+                {
+                    Interlocked.Increment(ref sayacBir);
+                }
+                else
+                {
+                    Interlocked.Increment(ref sayacSıfır);
+                }
+            }
+
+        }
+
+
+        //------------------------------ Semaphore
+
+        static int semSayac = 5; // thread poola girecek thread sayısı
+      
+        static void WaitONE() //kendi WaitOne ve Relase metotlarımız yazdık
+        {
+            while (semSayac <= 0) Interlocked.Add(ref semSayac, -1);
+        }
+        static void RELASE()
+        {
+            Interlocked.Add(ref semSayac, +1);
+        }
+
+        static Semaphore sem = new Semaphore(0,3); // başlangıçta kritik alanda 0 thread olacak daha sonra ise kritik alana 3 tane thread alabileceğiz.
+        static void yazSem()
+        {
+
+            Console.WriteLine(Thread.CurrentThread.Name + "Bekliyor");
+            //sem.WaitOne(); // semaphore girme izni
+            WaitONE();
+            Console.WriteLine("Semaphore İçerisindeyiz");
+            Thread.Sleep(5000);
+
+            //sem.Release();
+            RELASE();
+            Console.WriteLine("Semaphoredan Çıktık");
+        }
+
+
+        //------------------ AutoResetEvent BURASI FİNAL KONUSU
+        static void MyReadThreadProc()
+        {
+            while (true)
+            {
+                myResetEvent.WaitOne();
+                Console.WriteLine(number);
+            }
+        }
+        static AutoResetEvent myResetEvent = new AutoResetEvent(false);
+        static int number;
+
         static void Main(string[] args)
         {
 
-
             #region Thread oluşturma
-            Thread t = new Thread(yaz); // t --> child thread
-            t.Start();
-            Thread[] ts = new Thread[10];
+            //Thread t = new Thread(yaz); // t --> child thread
+            //t.Start();
+            //Thread[] ts = new Thread[10];
             /*
             for (int i = 0; i < 1000; i++)
             {
@@ -155,6 +243,21 @@ namespace İşletimSistemleri
                    th[i].Join(); //Kritik Alan
                }
                Console.WriteLine("int:" + sharedInt); */
+
+            // race condition 2. örnek
+            /* Thread t1 = new Thread(arttir);
+             Thread t2 = new Thread(arttir);
+
+             // Thread'leri başlatıyoruz
+             t1.Start();
+             t2.Start();
+
+             // İki thread'in de bitmesini bekliyoruz
+             t1.Join();
+             t2.Join();
+
+             Console.WriteLine("Sonuç: " + arttirSayac);*/
+
             #endregion
 
             #region THREAD UNSAFE kod
@@ -383,12 +486,79 @@ namespace İşletimSistemleri
             Console.WriteLine("Main Thread Çıkıyor");*/
             #endregion
 
+            #region Semaphore V2
+
+            /* for (int i = 0; i < 6; i++)
+             {
+                 Thread t = new Thread(yazSem);
+                 t.Name = "thread"+ i;
+                 t.Start();
+             }*/
+
+
+            #endregion
+
+            #region Interlocked
+
+
+            /*Thread t1 = new Thread(arttirInterlocked);
+            Thread t2 = new Thread(arttirInterlocked);
+
+            // Thread'leri başlatıyoruz
+            t1.Start();
+            t2.Start();
+
+            // İki thread'in de bitmesini bekliyoruz
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine("Sonuç: " + sayac);*/
+
+            #endregion
+
+            #region Interlocked Final Sınav Sorusu (2022)
+
+            // 100000 elemanlı dizi var. bu dizi 1 ve 0 lardan oluşmakta -> [1,1,0,1,1,...] 0 -> DOLU // 1 -> BOŞ
+            // her eleman bir clustera bakmakta
+            // dizinin doluluk oranını 4 thread ile bul.
+
+            int[] dizi = new int[100];
+            Random random = new Random();
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                dizi[i] = random.Next(2); // 0 veya 1 değeri alır
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(dizi[i]);
+            }
+            Thread th1 = new Thread(() => doluBosKontrol(dizi, 0,24));
+            Thread th2 = new Thread(() => doluBosKontrol(dizi, 25, 49));
+            Thread th3 = new Thread(() => doluBosKontrol(dizi, 50,74));
+            Thread th4 = new Thread(() => doluBosKontrol(dizi,75,99));
+
+            th1.Start();
+            th2.Start();
+            th3.Start();
+            th4.Start();
+
+            th1.Join();
+            th2.Join();
+            th3.Join();
+            th4.Join();
+
+            Console.WriteLine("Sıfır:"+ sayacSıfır);
+            Console.WriteLine("Bir:"+ sayacBir);
+
+            #endregion
+
 
             //threadlere isim verilebiir.
             //th.Name="racecondition"+i.ToString();
             //bütün threadler foreground dır.
-            //background thread main thread görevini tamammladığı zaman otomatik olarak kapatılır. Process kapanmaz
-            //ancak mai thread foreground thread.
+            //background thread main thread görevini tamamladığı zaman otomatik olarak kapatılır. Process kapanmaz
+            //ancak main thread foreground thread.
             //foreground threadler backgrounda dönüştürüldüğü zaman ikisi arassındaki tek fark background un otomatik olarak kapatması.
             //herbir thread 1 defa kullanılır.
 
@@ -397,7 +567,63 @@ namespace İşletimSistemleri
             //Kiliti kim attıysa o açar. Semaphoreda isteyen herkes açar. 
             //Çok yavaş. lock dan 50 kat civarında yavaş.
 
+            // Console.ReadLine();
+
+
+            //Thread thr = new Thread((object obj) => { Console.WriteLine(obj); });
+
+            // thr.Start();
+
+            // thr.Join();
+
+            //thr.Start();
+
+
+
+
+            /* Thread myReaderThread = new Thread(MyReadThreadProc);
+             myReaderThread.Name = "ReaderThread"; 
+             myReaderThread.Start();
+             for (int i = 1; i <= 3; i++)
+             {
+                 Console.WriteLine(i);
+                 number = i;
+                 myResetEvent.Set();
+                 Thread.Sleep(1);
+             }*/
+
+
+            /* object locker1 = new object();
+             object locker2 = new object();
+             Console.WriteLine("1");
+             new Thread(() =>
+             {
+                 lock (locker2)
+                 {
+                     Console.WriteLine("2");
+                     Thread.Sleep(2000);
+                     lock (locker1) ;
+                     Console.WriteLine("3");
+                 }
+             }).Start();
+             lock (locker2) // locker1
+             {
+                 Console.WriteLine("4");
+                 Thread.Sleep(2000);
+                 lock (locker2) ; // locker2
+                 Console.WriteLine("5");
+             }
+             Console.WriteLine("6");*/
+
+
+
+
             Console.ReadLine();
+
+
+
+
         }
+        
     }
 }
